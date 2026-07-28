0890cc892ec6907d0911bad4b06856ea0090a5ec
NagaramGaayathri <your-email@gmail.com>
2026-07-28 16:46:28 +0530

    Completed Week7 Angular Student Course Portal

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
-describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, student-course-portal');
-  });
-});
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'student-course-portal';
+}
